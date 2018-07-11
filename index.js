c384786f83524b4f1157ba4654b9deca6370900d
KERIOU Samir <samir.keriou@gendarmerie.interieur.gouvfr>
2018-07-11 15:59:38 +0200

    fix bug lncr

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ const path = require('path');
 module.exports = {
     init: function(_app) {
         app = _app;
-
-        //add all parser in directory ./parsers
-        let files = fs.readdirSync(__dirname+'/lib/parsers');
-        files.forEach(function(f) {
-           let i = path.basename(f, '.js');
-           i = i.replace(/_/g,'');
-           let p = __dirname+'/lib/parsers/'+f;
-           app.parsers[i] = p;
-        });
+        try {
+            //add all parser in directory ./parsers
+            let files = fs.readdirSync(__dirname + '/lib/parsers');
+            files.forEach(function (f) {
+                let i = path.basename(f, '.js');
+                i = i.replace(/_/g, '');
+                let p = __dirname + '/lib/parsers/' + f;
+                app.parsers[i] = p;
+                console.log(i, p);
+            });
+        } catch(e) {
+            console.log('parsing lib spec error', e);
+        }
     }
 };
 
